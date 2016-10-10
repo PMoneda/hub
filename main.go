4b13573cb23665b035ad885246392b48f01a60ea
Philippe Moneda <philippemoneda@gmail.com>
2016-10-09 21:51:45 -0300

    removing Hello Hub

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 //Package main is the entry point to Hub parser
 package main
 
-import (
-	"fmt"
-
-	"github.com/PMoneda/hub/workflow"
-)
+import "github.com/PMoneda/hub/workflow"
 
 const (
 	test1 = "./syntax/test/test1.hub"
 )
 
 func main() {
-	fmt.Println("Hello Hub")
 	var flow workflow.Workflow
 	//flow.Lex(test1).BuildAst().Print().Compile().PrintAsm()
 	flow.Lex(test1).BuildAst().Compile().PrintAsm()
